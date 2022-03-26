4ff495f2efb10f7969314a72d201fde4dfddc6e6
allenjoshua16 <89775169+allenjoshua16@users.noreply.github.com>
2022-03-26 23:52:19 +0530

    Update AutomatedMsg.py

--- AutomatedMsg.py
+++ AutomatedMsg.py
@@ -2,7 +2,7 @@ import pywhatkit
 import win32clipboard
 
 """SENDING AUTOMATED WHATSAPP MESSAGE TO A SINGLE CHAT"""
-# pywhatkit.sendwhatmsg("+91 7977807180",
+# pywhatkit.sendwhatmsg("+91 phone_number",
 #                       "Hello..!",
 #                       17,0o2)
 
@@ -16,7 +16,7 @@ import win32clipboard
 # pywhatkit.search("LIONEL MESSI")
 
 """SENDING A AUTOMATED MESSAGE ON WHATSAPP GROUP"""
-# pywhatkit.sendwhatmsg_to_group("DHG53GmzF6bH7KXl54rHJx", "Tumlog mai se koi PTA mai baita tha?", 17, 0o7)
+# pywhatkit.sendwhatmsg_to_group("Group_code", "Hello Guyss?", 17, 0o7)
 
 
 """SENDING A AUTOMATED IMAGE USING PYWHATKIT
@@ -28,11 +28,11 @@ we can also use (\\) before starting a string to let know that it is a string
 if you give the boolean value TRUE.. then whatsapp will close immediately after sending the message
 So always put FALSE """
 #
-# pywhatkit.sendwhats_image("+91 7977807180",
-#                           "C:/Users/hp/PycharmProjects/Python_mini/Operation_Kingfish_2013_group_crop.png",
+# pywhatkit.sendwhats_image("+91 phone_number",
+#                           "C:/Users/hp/PycharmProjects/Python_mini/Operation_Kingfish_2013_group_crop.png",  # this is where you give the path of the image
 #                           "This is a image sent by Pycharm to a whatsapp group",
 #                           10, False, 2)
 #
 # """ Automating messages """
 
-pywhatkit.show_history()
+pywhatkit.show_history()  # this shows the hostory of the messages sent 
